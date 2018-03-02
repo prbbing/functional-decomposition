28006fb3fd6a2466eaff9327980251c63b27d619
Ryan Christopher Edgar <ryan.christopher.edgar@cern.ch>
2018-03-02 16:41:00 -0500

    modified setup_bash.sh for Mac compatibility.

--- bin/setup_bash.sh
+++ bin/setup_bash.sh
@@ -4,19 +4,20 @@ TOPDIR=`dirname ${BINDIR}`
 
 if [ ".$QUIET" == "." ]
 then
-    echo -e
-    echo -e "\e[30m\e[107mIMPORTANT\e[0m: make sure you __source__ this script"
-    echo -e "rather than execute it:"
-    echo -e
-    echo -e "\e[1m    yourprompt$ . \$FD_PATH/setup_bash.sh\e[0m"
-    echo -e
-    echo -e "otherwise the paths will not be correctly set.  If you'd like to"
-    echo -e "silence this annoying message in the future, source the script"
-    echo -e "like this:"
-    echo -e
-    echo -e "\e[1m    yourprompt$ QUIET=1 . \$FD_PATH/setup_bash.sh\e[0m"
-    echo -e
-    echo -e "Python package dependency check:"
+    printf
+    printf "\e[30m\e[107mIMPORTANT\e[0m: make sure you __source__ this script\n"
+    printf "rather than execute it:\n"
+    printf "\n"
+    printf "\e[1m    yourprompt$ . \$FD_PATH/setup_bash.sh\e[0m\n"
+    printf "\n"
+    printf "otherwise the paths will not be correctly set.  If you'd like to\n"
+    printf "silence this annoying message in the future, source the script\n"
+    printf "like this:"
+    printf "\n"
+    printf "\e[1m    yourprompt$ QUIET=1 . \$FD_PATH/setup_bash.sh\e[0m\n"
+    printf "\n"
+
+    printf "Python package dependency check:\n"
     for PACKAGE in numpy scipy numexpr matplotlib ROOT
     do
         VERSION=$(python -c "import ${PACKAGE}; print ${PACKAGE}.__version__" 2>/dev/null )
@@ -28,11 +29,11 @@ then
             printf "%12s (%b)\n" "${PACKAGE}" "\e[32mfound ${VERSION}\e[0m"
         fi
     done
-    echo -e
-    echo -e "ROOT is __optional__, and is required only to import Tree objects from"
-    echo -e "  .root files.  If ROOT is not installed, you can still import .csv"
-    echo -e "  files."
-    echo
+    printf "\n"
+    printf "ROOT is __optional__, and is required only to import Tree objects from\n"
+    printf "  .root files.  If ROOT is not installed, you can still import .csv\n"
+    printf "  files."
+    printf "\n"
 fi
 
 [[ ":$PATH:"       != *${BINDIR}* ]] && export       PATH="${BINDIR}:${PATH}"
