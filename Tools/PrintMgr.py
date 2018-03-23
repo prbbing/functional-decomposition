65de094c4615630684c70a08581595f8a5e4d497
Ryan Christopher Edgar <ryan.christopher.edgar@cern.ch>
2018-03-23 11:29:37 -0400

    Enhanced comments on PrintMGr.

--- Tools/PrintMgr.py
+++ Tools/PrintMgr.py
@@ -1,19 +1,38 @@
+'''
+PrintMgr: functions for easy, consistent status printing to the screen with
+ANSI colors.
+
+PrintMgr is intended to print neatly-formatted status blocks to the screen,
+which are periodically updated (e.g. current fit parameters during a fit).
+
+
+pnew               : start a new status block
+pstage             : start a new status block with a nice header
+                     this is a decorator, and the decorated function is
+                     expected to do its own printing.
+
+pini / pdot / pend : new dotline / print dot / end dotline
+pstr               : new status string
+prst               : reset to beginning of block
+
+'''
+
 import sys, time
 
 #############
 _verbose  = True
-_Ngap     = 32          # Number of dots per line
-_Ndot     = 0           # Number of calls to _pdot
-_Nline    = 0           # number of lines (of dots) that have been printed
-_Nint     = 1           # print a dot every Nint calls
-_Tloc     = 54          # Column to print times
-_Ti       = time.time() # Starting time.
+_Ngap     = 32            # Number of dots per line
+_Ndot     = 0             # Number of calls to _pdot
+_Nline    = 0             # number of lines (of dots) that have been printed
+_Nint     = 1             # print a dot every Nint calls
+_Tloc     = 54            # Column to print times
+_Ti       = time.time()   # Starting time.
 
-_str_bl  = "\x1b[2K\r\x1b[1m%20s\x1b[0m: "
-str_el   = "\x1b[2K\r\x1b[33;1m%20s\x1b[0m: "
-str_nl   = "\x1b[2K\r%20s: "
-_str_sta = "\x1b[2K\r\x1b[1m[\x1b[32m %s \x1b[39m]\x1b[0m" 
-_str_set = "\r\x1b[%dC"
+_str_bl  = "\x1b[2K\r\x1b[1m%20s\x1b[0m: "                  # Emph (bold)
+str_el   = "\x1b[2K\r\x1b[33;1m%20s\x1b[0m: "               # Emph (yellow)
+str_nl   = "\x1b[2K\r%20s: "                                # Normal (white)
+_str_sta = "\x1b[2K\r\x1b[1m[\x1b[32m %s \x1b[39m]\x1b[0m"  # Block header
+_str_set = "\r\x1b[%dC"                                     # Set cursor to loc
 
 #\x1b[2K\r == reset line
 
@@ -33,7 +52,7 @@ def _str_ar(pfx, ar, pfmt, afmt, plen):
           str += afmt % a
     return str
 
-def _pnew(str):
+def pnew(str):
     global _Nline
     if not _verbose: return
 
@@ -106,7 +125,7 @@ def pstage(str):
 
           if verbose:
               t1 = time.time()
-              _pnew(_str_sta % str)
+              pnew(_str_sta % str)
           r   = func(*arg, **kw)
 
           if verbose:
